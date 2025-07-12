aeedd4f557c9ced084cfc026042225c75d41cf3e
StarLordiRussia <danila.kalashnik.05@gmail.com>
2025-07-12 18:54:32 +0300

    Перезапись содержимого репозитория

--- AnimeVault/ViewModels/ViewModelBase.cs
+++
@@ -1,8 +0,0 @@
-﻿using ReactiveUI;
-
-namespace AnimeVault.ViewModels
-{
-    public abstract class ViewModelBase : ReactiveObject
-    {
-    }
-}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace AnimeVault.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
